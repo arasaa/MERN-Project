7224ef9ca9eb4b89e5b0d9834f29aecac581ac18
araz saydu <arazsaydu@gmail.com>
2022-01-31 09:15:51 +0100

    MERN

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import postRoutes from './routes/posts.js';
+
+
+const app = express();
+
+app.use(bodyParser.json({limit: "32mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "32mb", extended: true}));
+app.use(cors());
+
+//localhost:5000/posts
+app.use('/posts', postRoutes)
+
+const CONNECTION_URL = 'mongodb+srv://Mern:me123@cluster0.pj5pk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONNECTION_URL)
+.then(() => 
+    app.listen(PORT, () => console.log(`server running on port: ${PORT}`)))
+.catch(err => console.log(err.message))
---
+++ server/routes/posts.js
@@ -0,0 +1,13 @@
+//this file is for Routes and their logic will be in the posts.js in controller folder
+import express from "express";
+import {getPosts, createPost} from "../controllers/posts.js"
+
+const router = express.Router();
+
+
+
+//sending respons for 5000/posts
+router.get('/', getPosts);
+router.get('/', createPost);
+
+export default router;
