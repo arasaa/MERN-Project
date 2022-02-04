f74535f6c0d7da3278437da9b15dce925e14cdb1
araz saydu <arazsaydu@gmail.com>
2022-02-04 08:12:52 +0100

    connecting to backend doesnt work

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core'
+
+
+import {getPosts} from './actions/posts'
+import {useDispatch} from 'react-redux'
 import ViewMe from './images/ViewMe.jpg';
 import Posts from './components/Posts/Posts'
 import Form from './components/Form/Form'
+import useStyles from './styles';
+
+
+
 
 const App = () => {
+    const classes = useStyles()
+    const dispatch = useDispatch()
+
+    useEffect(() =>{
+        dispatch(getPosts)
+    }, [dispatch])
     return(
        <Container maxWidth="lg">
-           <AppBar position='static'color='inherit' >
-               <Typography variant='h2' align='center'>ViewMe</Typography>
-               <img src={ViewMe} alt='ViewMe' height='60' />
+           <AppBar className={classes.appBar} position='static'color='inherit' >
+               <Typography className={classes.heading} variant='h2' align='center'>ViewMe</Typography>
+               <img className={classes.image} src={ViewMe} alt='ViewMe' height='60' />
 
            </AppBar>
            <Grow in>
                <Container>
-                   <Grid container justify='space-between' alignItems='stretch' spacing='4' >
+                   <Grid container justifyContent='space-between' alignItems='stretch' spacing={4} >
                        <Grid item xs={12} sm={7}>
                             <Posts />
                        </Grid>
--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,10 +1,93 @@
-import React from 'react';
+import React, { useState } from "react";
+import useStyles from "./styles";
+import { TextField, Button, Typography, Paper } from "@material-ui/core";
+import FileBase from 'react-file-base64'
+import {useDispatch} from 'react-redux'
+import {createPost} from '../../actions/posts'
 
 
 const Form = () => {
-    return(
-    <h1>Form</h1>
-    )
-}
+  const [postData, setPostData] = useState({
+    creator: "",
+    title: "",
+    message: "",
+    tags: "",
+    selectedFile: "",
+  });
+  const classes = useStyles();
+  const dispatch = useDispatch()
+
+  const handleSubmit = (e) => {
+    //to not refresh the browser
+    e.preventDefault();
+    dispatch(createPost(postData))
+  };
+
+  const clear = () => {
+
+  }
+
+  return (
+    <Paper className={classes.paper}>
+      <form
+        autoComplete="off"
+        noValidate
+        className={`${classes.root}${classes.form}`}
+        onSubmit={handleSubmit}
+      >
+      <Typography variant="h6">creating a post</Typography>
+      <TextField
+        name="creator"
+        variant="outlined"
+        label="creator"
+        fullwidth="true"
+        value={postData.creator}
+        onChange={(e) =>
+          setPostData({ ...postData, creator: e.target.value })
+        }
+      />
+       <TextField
+        name="title"
+        variant="outlined"
+        label="Title"
+        fullwidth="true"
+        value={postData.title}
+        onChange={(e) =>
+          setPostData({ ...postData, title: e.target.value })
+        }
+      />
+       <TextField
+        name="message"
+        variant="outlined"
+        label="Message"
+        fullwidth="true"
+        value={postData.message}
+        onChange={(e) =>
+          setPostData({ ...postData, message: e.target.value })
+        }
+      />
+       <TextField
+        name="tags"
+        variant="outlined"
+        label="Tags"
+        fullwidth="true"
+        value={postData.tags}
+        onChange={(e) =>
+          setPostData({ ...postData, tags: e.target.value })
+        }
+      />
+      <div className={classes.fileInput}>
+          <FileBase
+            type='file'
+            multiple={false}
+            onDone={({base64}) => setPostData({ ...postData, selectedFile: base64 })}
+          />
+      </div>
+      <Button className={classes.buttonSubmit} variant='contained' color='primary' size='large' type='submit' fullwidth='true'>submit</Button>
+      <Button variant='contained' color='secondary' size='small' type='submit' onClick={clear} fullwidth='true'>clear</Button>
+      </form>
+    </Paper>
+  );
+};
 
 export default Form;
