9bd16d0a5ea590e63a6b2755e64cfd3606ae7aab
araz saydu <arazsaydu@gmail.com>
2022-02-10 09:53:20 +0100

    updatePost

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,5 @@
 import PostMessage from '../models/postMessage.js'
+import mongoose  from 'mongoose';
 
 
 export const getPosts = async (req, res) =>{
@@ -22,4 +23,18 @@ export const createPost = async (req, res) => {
     } catch (error) {
         res.status(409).json({message: error.message})
     }
+}
+
+//updating method 
+//this function is depends with routes/Posts
+//(router.dispatch('./:id', updatePost))
+export const updatePost =  async (req, res) => {
+    const {id} = req.params
+    const post = req.body
+    //chicking if the _id is from Mongoose
+    if(!mongoose.Types.Object.isValid(_id)) return res.status(404).send('no post with that id');
+
+    //updating Message logic
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {new: true});
+    res.jeson(updatedPost)
 }
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,6 +1,6 @@
 //this file is for Routes and their logic will be in the posts.js in controller folder
 import express from "express";
-import {getPosts, createPost} from "../controllers/posts.js"
+import {getPosts, createPost, updatePost} from "../controllers/posts.js"
 
 const router = express.Router();
 
@@ -9,5 +9,7 @@ const router = express.Router();
 //sending respons for 5000/posts
 router.get('/', getPosts);
 router.post('/', createPost);
+//the calling of the function controller Posts
+router.patch('/:id', updatePost)
 
 export default router;
